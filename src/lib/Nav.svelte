de2cc624583374abdae8b8dc8abe0aefc6d4cc69
Script Raccoon <scriptraccoon@gmail.com>
2025-03-17 23:54:03 +0100

    add nav

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,19 @@
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/folders">Folders</a>
+		</li>
+		<li>
+			<a href="/carpet">Carpet</a>
+		</li>
+		<li>
+			<a href="/fibonacci">Fibonacci</a>
+		</li>
+		<li>
+			<a href="/tiling">Tiling</a>
+		</li>
+	</ul>
+</nav>
