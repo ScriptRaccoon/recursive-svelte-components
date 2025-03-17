d846c0958dada3c6c01d2bfffde0d86c5975f486
Script Raccoon <scriptraccoon@gmail.com>
2025-03-18 00:08:37 +0100

    add generic range input component

---
+++ src/lib/RangeInput.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	type Props = {
+		value: number;
+		label: string;
+		min: number;
+		max: number;
+	};
+
+	let { value = $bindable(), label, min, max }: Props = $props();
+</script>
+
+<div>
+	<label>
+		<input type="range" bind:value {min} {max} aria-label={label} />
+		<span aria-hidden={true}>
+			{value}
+		</span>
+	</label>
+</div>
+
+<style>
+	div,
+	label {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	label {
+		gap: 0.5rem;
+	}
+
+	input {
+		accent-color: yellow;
+	}
+</style>
