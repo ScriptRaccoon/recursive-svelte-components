b541593f0b13fab306a950b8bc2b2eeaaf6f1b31
Script Raccoon <scriptraccoon@gmail.com>
2025-03-18 00:12:37 +0100

    add example "binary tiling"

---
+++ src/routes/tiling/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import RangeInput from '$lib/RangeInput.svelte';
+	import Binary from './Binary.svelte';
+
+	let depth = $state(4);
+</script>
+
+<h1>Binary tiling</h1>
+
+<RangeInput bind:value={depth} min={0} max={6} label="depth of binary tiling" />
+
+<div class="wrapper">
+	<Binary {depth} />
+</div>
+
+<style>
+	.wrapper {
+		margin-top: 1rem;
+		width: min(90vw, 500px);
+		aspect-ratio: 1;
+		margin-inline: auto;
+	}
+</style>
---
+++ src/routes/tiling/Binary.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Binary from './Binary.svelte';
+
+	type Props = {
+		depth: number;
+	};
+
+	let { depth }: Props = $props();
+</script>
+
+<div class="container">
+	{#if depth > 0}
+		<div class="box">
+			<Binary depth={depth - 1} />
+		</div>
+		<div class="box">
+			<Binary depth={depth - 1} />
+		</div>
+		<div class="box">
+			<Binary depth={depth - 1} />
+		</div>
+		<div class="box">
+			<Binary depth={depth - 1} />
+		</div>
+	{:else if depth === 0}
+		<div class="box full"></div>
+	{/if}
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		height: 100%;
+		display: grid;
+		grid-template-rows: 1fr 1fr;
+		grid-template-columns: 1fr 1fr;
+		gap: 0.25rem;
+		background-color: #000;
+	}
+
+	.box {
+		background-color: skyblue;
+	}
+
+	.box.full {
+		grid-row: 1 / span 2;
+		grid-column: 1 / span 2;
+	}
+</style>
