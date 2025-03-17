ae9cdc0fb5ef3b1e468c3ff70941dc0ae9d917ac
Script Raccoon <scriptraccoon@gmail.com>
2025-03-18 00:10:28 +0100

    add example "fibonacci sequence"

---
+++ src/routes/fibonacci/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Fibonacci from './Fibonacci.svelte';
+	import RangeInput from '$lib/RangeInput.svelte';
+
+	let fiboIndex = $state(8);
+	let fiboWrapper = $state<HTMLElement | null>(null);
+
+	const countBlocks = (node: HTMLElement, _key: number) => {
+		const blocks = Array.from(node.querySelectorAll('.block'));
+		for (let i = 0; i < blocks.length; i++) {
+			blocks[i].innerHTML = String(i + 1);
+		}
+	};
+
+	$effect(() => {
+		if (fiboWrapper) countBlocks(fiboWrapper, fiboIndex);
+	});
+</script>
+
+<h1>Fibonacci sequence</h1>
+
+<RangeInput bind:value={fiboIndex} label="index for fibonacci sequence" min={0} max={12} />
+
+<div class="wrapper" bind:this={fiboWrapper}>
+	<Fibonacci index={fiboIndex} />
+</div>
+
+<style>
+	.wrapper {
+		margin-inline: 2rem;
+		margin-block: 1rem 2rem;
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+	}
+
+	.wrapper :global(.block) {
+		width: 2rem;
+		height: 2rem;
+		background-color: steelblue;
+		border-radius: 0.5rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+	}
+</style>
---
+++ src/routes/fibonacci/Fibonacci.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Fibonacci from './Fibonacci.svelte';
+	type Props = {
+		index: number;
+	};
+
+	let { index }: Props = $props();
+</script>
+
+{#if index === 1}
+	<div class="block"></div>
+{:else if index >= 2}
+	<Fibonacci index={index - 1} />
+	<Fibonacci index={index - 2} />
+{/if}
+
+<style>
+	.block {
+		background-color: steelblue;
+	}
+</style>
