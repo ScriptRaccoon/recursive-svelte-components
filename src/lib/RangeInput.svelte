32f4b95ab64983b6c740ea56a700754ab1b12082
Script Raccoon <scriptraccoon@gmail.com>
2025-03-18 23:20:40 +0100

    rework range input

--- src/lib/RangeInput.svelte
+++ src/lib/RangeInput.svelte
@@ -10,29 +10,19 @@
 	let { value = $bindable(), label, min, max, step = 1 }: Props = $props()
 </script>
 
-<div>
-	<label>
-		<input type="range" bind:value {min} {max} {step} aria-label={label} />
-		<span aria-hidden={true} class="value">
-			{value}
-		</span>
-	</label>
-</div>
+<label>
+	<input type="range" bind:value {min} {max} {step} aria-label={label} />
+	<span aria-hidden={true} class="value">
+		{value}
+	</span>
+</label>
 
 <style>
-	div,
 	label {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-	}
-
-	label {
 		gap: 0.5rem;
-	}
-
-	input {
-		accent-color: yellow;
 	}
 
 	.value {
